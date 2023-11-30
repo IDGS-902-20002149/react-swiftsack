e5350dfd9f9cf67fbc7b58f236b8c6427285fd94
IDGS-902-20001677 <20001677@alumnos.utleon.edu.mx>
2023-11-30 12:08:17 -0600

    Login y registro de usuario(Con localStorage)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./modulos/home/Home";
 import Navbar from "./Navbar";
+import AuthComponent from "./modulos/login/AuthComponent";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<AuthComponent />} /> 
         </Routes>
       </BrowserRouter>
     </>
